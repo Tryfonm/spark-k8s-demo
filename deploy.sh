59b4a7608781e4ecb57870337a4dc091f1a28058
Tryfon <53997456+Tryfonm@users.noreply.github.com>
2023-09-20 18:40:32 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-k3d cluster delete --all                                                       
+                                          
 k3d cluster create --port 8082:30080@agent:0 -p 8081:80@loadbalancer --agents 2
 docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube/spark-shared-volume
 docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube/spark-event-logs
