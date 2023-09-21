1b4e5cde8041210450d88d65f4b91bcf3a32280d
Tryfonm <michalopoulos_tryfon@outlook.com.gr>
2023-09-21 16:37:45 +0200

    fixed

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Project Name
 
 ### Introduction
-This project deploys a Kubernetes cluster using k3d and sets up a Spark cluster with a master, worker nodes, and a driver. It also includes a bash script, `submit-example-job.sh`, which runs an existing example from the Apache Spark project.
+This project deploys a Kubernetes cluster using k3d and sets up a Spark cluster with a master, worker nodes, and a driver.
 
 ### Prerequisites
 Before you begin, ensure you have the following installed:
@@ -14,7 +14,8 @@ Run the deployment script to create the Kubernetes cluster and Spark components:
 ```
 The deployment script sets up a master, a specified number of worker nodes (you can adjust this in manifests/spark-worker.yaml by changing the replica value), and the Spark driver.
 Logging is enabled and the logs are stored on k3d's agent-0 under `/tmp/kube/spark-event-logs` and mapped on a persistentvolume that is mounted under `/opt/spark/event-logs` for both the `driver`, that stores them and the `spark-history-server`, that parses the folder; the UI spark-history-server is exposed using cluster-ip as a service and an ingress, and can be accessed through `localhost:8081`
-Use the provided submit-example-job.sh script to run an existing example provided by the Apache Spark project:
+For demonstration purposes the scripts for the submission of two spark-jobs are being provided (the first one is provided by the Apache spark project and the other one is cloned from github):
 ```
-./submit-example-job.sh
+./submit-example-job-local.sh
+./submit-example-job-github.sh
 ```
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-                                          
+
 k3d cluster create --port 8082:30080@agent:0 -p 8081:80@loadbalancer --agents 2
 docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube/spark-shared-volume
 docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube/spark-event-logs
@@ -20,18 +20,18 @@ kubectl apply -f manifests/spark-driver.yaml
 
 # -------------------------------------------------------- #
 
-# (Optional) Add prometheus/grafana/loki for logging
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add stable https://charts.helm.sh/stable
-kubectl create namespace prometheus
-helm install prometheus-community/kube-prometheus-stack --generate-name --namespace prometheus
+# # (Optional) Add prometheus/grafana/loki for logging
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo add stable https://charts.helm.sh/stable
+# kubectl create namespace prometheus
+# helm install prometheus-community/kube-prometheus-stack --generate-name --namespace prometheus
 
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-kubectl create namespace loki-stack
-helm upgrade --install loki --namespace=loki-stack grafana/loki-stack
+# helm repo add grafana https://grafana.github.io/helm-charts
+# helm repo update
+# kubectl create namespace loki-stack
+# helm upgrade --install loki --namespace=loki-stack grafana/loki-stack
 
-# After grafana is running, run the following (screen -dmS prometheus ...):
-kubectl -n prometheus port-forward \
-$(kpod -n prometheus --no-headers | grep grafana | awk '{print $1}') 3000
-Dont forget to add to source: http://loki.loki-stack:3100
+# # After grafana reaches 'Running' state, run the following (screen -dmS prometheus ..):
+# kubectl -n prometheus port-forward \
+# $(kpod -n prometheus --no-headers | grep grafana | awk '{print $1}') 3000
+# Dont forget to add to source: http://loki.loki-stack:3100
